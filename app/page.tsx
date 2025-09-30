a704828df5236da939466b6d50132bffddd6bf73
KoniChivaJS <mredik200@gmail.com>
2025-09-30 22:05:54 +0300

    hooks: Intersection + Zustand

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <div className="flex-1">
             <div className="flex flex-col gap-12">
               <ProductsGroupList
-                title="Pizzas"
+                title="Pizza"
                 items={[
                   {
                     id: 1,
@@ -57,6 +57,41 @@ export default function Home() {
                   },
                 ]}
                 categoryId={1}
+              />
+
+              <ProductsGroupList
+                title="Combo"
+                items={[
+                  {
+                    id: 1,
+                    name: "Cheese Pizza",
+                    imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrgh6Xx8CBnZM8NPOmIEOwqV_KK6PnE1pluA&s",
+                    items: [{ price: 9.9 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Cheese Pizza",
+                    imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrgh6Xx8CBnZM8NPOmIEOwqV_KK6PnE1pluA&s",
+                    items: [{ price: 9.9 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Cheese Pizza",
+                    imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrgh6Xx8CBnZM8NPOmIEOwqV_KK6PnE1pluA&s",
+                    items: [{ price: 9.9 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Cheese Pizza",
+                    imageUrl:
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrgh6Xx8CBnZM8NPOmIEOwqV_KK6PnE1pluA&s",
+                    items: [{ price: 9.9 }],
+                  },
+                ]}
+                categoryId={2}
               />
             </div>
           </div>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,36 +1,41 @@
+"use client";
+
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
 import React from "react";
 
 interface Props {
   className?: string;
 }
 
-const cats = [
-  "Pizza",
-  "Combo",
-  "Snacks",
-  "Cocktails",
-  "Coffee",
-  "Drinks",
-  "Deserts",
+const categories = [
+  { id: 1, name: "Pizza" },
+  { id: 2, name: "Combo" },
+  { id: 3, name: "Snacks" },
+  { id: 4, name: "Cocktails" },
+  { id: 5, name: "Coffee" },
+  { id: 6, name: "Drinks" },
+  { id: 7, name: "Deserts" },
 ];
-const activeIndex = 0;
 
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
+
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map((cat, index) => (
+      {categories.map(({ name, id }, index) => (
         <a
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            activeIndex === index &&
+            categoryActiveId === id &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
           key={index}
+          href={`/#${name}`}
         >
-          <button>{cat}</button>
+          <button>{name}</button>
         </a>
       ))}
     </div>
