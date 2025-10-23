d806b5b02a59180cb78488f7812a98b9323c5213
KoniChivaJS <mredik200@gmail.com>
2025-10-23 18:26:36 +0300

    create product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,55 @@
-export default function ProductPage({
+import { Container, ProductImage, Title } from "@/components/shared";
+import { GroupVariants } from "@/components/shared/group-variants";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>{id}</div>;
+  const product = await prisma.product.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={40} className="" />
+        <div className="w-[490px] bg-[#FCFCFC] p-7">
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sed
+            tenetur rerum alias exercitationem excepturi ea provident soluta, in
+            voluptate magnam ab reiciendis, blanditiis molestias delectus
+            recusandae et id at!
+          </p>
+          <GroupVariants
+            items={[
+              {
+                name: "Small",
+                value: "1",
+              },
+              {
+                name: "Medium",
+                value: "2",
+              },
+              {
+                name: "Large",
+                value: "3",
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
